66df5247224e525e829b8747cd2e3dbe0fa5ce6f
speedy_ln <lnnkadimeng@gmail.com>
2016-08-23 08:48:36 +0200

    redirection << update, 80%

--- redirection.c
+++ redirection.c
@@ -11,6 +11,7 @@
 /* ************************************************************************** */
 
 #include "fortytwosh.h"
+#include <stdio.h>
 
 void	ft_redirect(t_main *w, t_env *env)
 {
@@ -60,10 +61,70 @@ void	redirection_lt(t_main *w, t_env *env)
 {
 	char	**coms;
 	int		fd[2];
+	int		fd2[2];
+	char	*line;
+	char	*ln;
+	pid_t	childpid;
+	char    readbuffer[80];
+	int		temp;
 
 	coms = ft_strsplit(w->line, '<');
 	fd[0] = open(ft_strrw(coms[1]), O_RDWR);
-	if (fd[0] == -1)
+	ln = (char *)malloc(sizeof(char *) * 1);
+	ln[0] = '\0';
+	if (ft_findstr("<<", w->line))
+	{
+		ft_putstr("heredoc> ");
+		while (get_next_line(0, &line))
+		{
+			if (ft_strcmp(line, ft_strrw(coms[1])) == 0)
+				break;
+			ln = ft_strjoin(ln, line);
+			ln = ft_strjoin(ln, "\n");
+			ft_putstr(line);
+			ft_putchar('\n');
+			ft_putstr("heredoc> ");
+		}
+		ln = ft_strjoin(ln, "\0");
+		if (pipe(fd2) != -1)
+		{
+			childpid = fork();
+			if(childpid == -1)
+			{
+				perror("fork");
+				ft_strcpy(w->line, " ");
+				ft_minishell(env, w);
+			}
+			else if(childpid == 0)
+			{
+				/* Child process closes up input side of pipe */
+				close(fd2[1]);
+				read(fd2[0], readbuffer, 1024);
+				ft_putendl(readbuffer);
+				close(fd2[0]);
+				ft_strcpy(w->line, coms[0]);
+				fd[1] = dup(STDIN_FILENO);
+				ft_putnbr(fd2[0]);
+				temp = dup2(fd2[0], STDIN_FILENO);
+				close(fd2[0]);
+				ft_minishell2(env, *w);
+				dup2(fd[1], STDIN_FILENO);
+				close(fd[1]);
+				ft_putnbr(temp);
+				exit(0);
+			}
+			else
+			{
+				/* Parent process closes up output side of pipe */
+				close(fd2[0]);
+				ft_putstr_fd(ln, fd2[1]);
+				close(fd2[1]);
+				wait(NULL);
+				exit(0);
+			}
+		}
+	}
+	else if (fd[0] == -1)
 	{
 		ft_putstr("File doesn't exist or cannot be opened.\n");
 		ft_strcpy(w->line, " ");
